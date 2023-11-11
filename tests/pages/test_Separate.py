2c64e6265908f2b0f7efe93d991d4d03ee50c3b6
fabiogra <fabiograsso1995@gmail.com>
2023-11-11 21:02:47 +0100

    test: add conftest, test_separation_mode

--- tests/pages/test_Separate.py
+++ tests/pages/test_Separate.py
@@ -1,7 +1,19 @@
-from streamlit.testing.v1 import AppTest
+import pytest
 
 
-def test_execution():
-    at = AppTest.from_file("app/pages/Separate.py")
-    at.run(timeout=10)
-    assert not at.exception
+def test_execution(separate_running_app):
+    assert not separate_running_app.exception
+
+
+@pytest.mark.parametrize(
+    "selection",
+    [
+        "Vocals & Instrumental (Low Quality, Faster)",
+        "Vocals & Instrumental (High Quality, Slower)",
+        "Vocals, Drums, Bass & Other (Slower)",
+        "Vocal, Drums, Bass, Guitar, Piano & Other (Slowest)",
+    ],
+)
+def test_separation_mode(selection, separate_running_app):
+    separate_running_app.selectbox(key="separation_mode").set_value(selection)
+    assert separate_running_app.selectbox(key="separation_mode").value == selection

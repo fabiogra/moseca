6f438bc0bd3bf44a60369e654a52e1560124a3d9
fabiogra <fabiograsso1995@gmail.com>
2023-11-11 14:46:53 +0100

    fix: typo

--- app_test.py
+++ app_test.py
@@ -2,6 +2,6 @@ from streamlit.testing.v1 import AppTest
 
 
 def test_execution():
-    at = AppTest.from_file("app/pages/separate.py")
+    at = AppTest.from_file("app/pages/Separate.py")
     at.run()
     assert not at.exception

02b40a7f2de44a0759b77f556ad5ff093694a8ed
fabiogra <fabiograsso1995@gmail.com>
2023-11-11 19:49:36 +0100

    test: increase timeout to 10 secs

--- app_test.py
+++ app_test.py
@@ -3,5 +3,5 @@ from streamlit.testing.v1 import AppTest
 
 def test_execution():
     at = AppTest.from_file("app/pages/Separate.py")
-    at.run()
+    at.run(timeout=10)
     assert not at.exception
